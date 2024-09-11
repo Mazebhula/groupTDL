f425bf62e5919824c91511e0c3e8ac9593238e2f
wtc Group178 <dzebudenzhe0@gmail.com>
2024-09-11 11:37:12 +0200

    Add files via upload

---
+++ login.py
@@ -0,0 +1,31 @@
+
+users = dict()
+def create_user():
+     import hashlib
+     from getpass import getpass
+     
+     username = input('Username: ')
+     password = hashlib.sha256(getpass('password: '  ).encode()).hexdigest()
+     #print(password)
+     users[username]=password
+     return
+ 
+def login():
+    import hashlib
+    from getpass import getpass
+    print("welcome please login below")
+    username = input('username: ')
+    pasid = hashlib.sha256(getpass('password: ').encode()).hexdigest()
+    if pasid== users[username]:
+        print('welcome')
+        return True
+    else:
+        print('username or password incorrect')
+        return 
+    
+value = input('type "new"" for new user: ')
+if value == 'new':
+    create_user()
+    login()
+else:
+    login()
